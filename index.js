e17fd3c512337a44f72a7e515bb73e767fd32a7d
ok_neosurance <ok_neosurance@ok-neosurance.local>
2019-05-28 18:04:50 +0200

    files pod v7

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import { NativeModules } from 'react-native';
 
 const { RNReactNativeIosLibraryNsr } = NativeModules;
 
-const Neosurance = RNReactNativeIosLibraryNsr;
+var Neosurance = RNReactNativeIosLibraryNsr;
 export default Neosurance;
