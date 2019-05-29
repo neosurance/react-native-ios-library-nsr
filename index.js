ac3c3738273f53d82860844bbeb6675bc0303331
ok_neosurance <ok_neosurance@ok-neosurance.local>
2019-05-29 10:10:29 +0200

    new version v1

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
+'use strict'
 import { NativeModules } from 'react-native';
 
-const { RNReactNativeIosLibraryNsr } = NativeModules;
-
-var Neosurance = RNReactNativeIosLibraryNsr;
-export default Neosurance;
+module.exports = NativeModules.RNReactNativeIosLibraryNsr;
